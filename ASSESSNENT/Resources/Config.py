2652e4127db48cdda23af3bc944cf27518bc1527
Fidhavazee <165985556+Fidhavazee@users.noreply.github.com>
2024-07-19 09:56:25 +0530

    Add files via upload
    Changed testcases to testcase folder

---
+++ ASSESSNENT/Resources/Config.py
@@ -0,0 +1,33 @@
+BASE_URL = "https://reqres.in"
+
+# GET request data
+GET_END_POINT= "api/users/2"
+GET_STATUS_CODE = 200
+
+# PUT request data
+PUT_END_POINT = "api/users/2"
+PUT_STATUS_CODE = 200
+PUT_DATA = {
+    "name": 
+        "silpa",
+    "job": 
+        "QA"
+    }
+
+# DELETE request data
+DELETE_END_POINT = "api/users/2"
+DELETE_STATUS_CODE = 204
+
+# POST request data
+POST_END_POINT = "api/users"
+POST_STATUS_CODE = 201
+POST_DATA = {
+    "name": 
+        "morpheus",
+    "job": 
+        "leader"
+    }
+
+
+
+
